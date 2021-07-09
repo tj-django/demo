e17a8ffa4e6134c315f2ee68f378892363c632ba
Tonye Jack <jtonye@ymail.com>
2021-07-09 14:07:27 -0400

    Created the test 04 migration.

--- models.py
+++ models.py
@@ -7,12 +7,15 @@ class TestModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=500)
+    dob = models.DateTimeField(blank=True, null=True)
     age = models.PositiveIntegerField(default=500)
 
     if django.VERSION < (2, 1):
         active = models.NullBooleanField()
     else:
         active = models.BooleanField(null=True)
+        
+    
 
     class Meta:
         verbose_name = _("test model")
