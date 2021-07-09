f270215a26d162a5250c5a1b4e89fcaa353dacec
Tonye Jack <jtonye@ymail.com>
2021-07-09 12:14:55 -0400

    Updated migration

--- migrations/0004_auto_20210708_1317.py
+++ migrations/0004_auto_20210708_1317.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('demo', 'testmodel_created_by'),
+        ('demo', '0003_testmodel_created_by'),
     ]
 
     operations = [
